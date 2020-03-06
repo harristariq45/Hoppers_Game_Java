5e0d365ef892597bcb8bd95138856a7a3be5bf31
Tariq <tariqhb1@egb070000022.lancs.local>
2020-03-06 15:02:40 +0000

    frame coded

---
+++ Board.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event; // action listener
+
+public class Board {
+
+    a = new JFrame();
+    a.setContentPane(content);
+    a.setVisible(true);
+    a.setTitle("HopperGame");
+    a.setSize(500,500);
+    a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    public static void main(String[] args) {
+        Board s=new Board();    
+    }
+
+}
---
+++ square.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event; // action listener
+
+public class Board {
+    JButton water;
+
+
+
+
+
+
+}
